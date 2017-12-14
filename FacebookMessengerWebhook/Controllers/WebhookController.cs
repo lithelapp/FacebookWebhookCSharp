2b699f9646a47e89a1e03e1174dd262bdc78b1ba
Eldric <eldricthli@hotmail.com>
2017-12-14 22:29:59 +0800

    first commit

---
+++ FacebookMessengerWebhook/Controllers/WebhookController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FacebookMessengerWebhook.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Webhook")]
+    public class WebhookController : Controller
+    {
+        /// <summary>
+        /// Endpoint for getting message
+        /// </summary>
+        /// <param name="request">Request sent when a user initiate a message</param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Post([FromBody]WebhookMessageRequest request)
+        {
+            if (request.Object == "page")
+            {
+                foreach (var entry in request.Entries)
+                {
+                    string pageID = entry.Id;
+
+                    foreach (var mEvent in entry.Messagings)
+                    {
+                        if (mEvent.Message != null)
+                        {
+                            Console.WriteLine("Message received: " + mEvent.Message.Text);
+                        }
+                    }
+                }
+                return Ok("EVENT_RECEIVED");
+            }
+            return NotFound();
+        }
+
+        /// <summary>
+        /// Endpoint for verifying webhook
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get([FromQuery(Name = "hub.mode")] string mode, [FromQuery(Name = "hub.challenge")] string Challenge, [FromQuery(Name = "hub.verify_token")] string VerifyToken)
+        {
+            string VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>";
+            if (mode == "subscribe" && VerifyToken == VERIFY_TOKEN)
+            {
+                Console.WriteLine("WEBHOOK_VERIFIED");
+                return Ok(Challenge);
+            }
+            else
+            {
+                return Forbid();
+            }
+        }
+    }
+}
---
+++ FacebookMessengerWebhook/FbMessenger.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace FacebookMessengerWebhook
+{
+    [DataContract]
+    public class WebhookMessageRequest
+    {
+        [Required]
+        [DataMember(Name = "object")]
+        public string Object { get; set; }
+        [Required]
+        [DataMember(Name = "entry")]
+        public List<Entry> Entries { get; set; }
+    }
+
+    [DataContract]
+    public class Entry
+    {
+        [Required]
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+        [Required]
+        [DataMember(Name = "time")]
+        public long Time { get; set; }
+        [Required]
+        [DataMember(Name = "messaging")]
+        public List<Messaging> Messagings { get; set; }
+    }
+
+    public class Messaging
+    {
+        [Required]
+        public User Sender { get; set; }
+        [Required]
+        public User Recipient { get; set; }
+        public long Timestamp { get; set; }
+        public MessageContent Message { get; set; }
+
+        public class User
+        {
+            [Required]
+            public string Id { get; set; }
+        }
+
+        [DataContract]
+        public class MessageContent
+        {
+            [Required]
+            [DataMember(Name = "mid")]
+            public string Mid { get; set; }
+            [DataMember(Name = "text")]
+            public string Text { get; set; }
+            [DataMember(Name = "quick_reply")]
+            public QuckyReplyContent QuickReply { get; set; }
+            public class QuckyReplyContent
+            {
+                public string Payload { get; set; }
+            }
+        }
+    }
+
+    [DataContract]
+    public class SendRequest
+    {
+        [DataMember(Name = "recipient")]
+        public RecipientObject Recipient { get; set; }
+        [DataMember(Name = "message")]
+        public MessageContent Message { get; set; }
+
+        [DataContract]
+        public class RecipientObject
+        {
+            [DataMember(Name = "id")]
+            public string Id { get; set; }
+            [DataMember(Name = "phone_number")]
+            public string PhoneNumber { get; set; }
+            //[DataMember(Name = "name")]
+            //public NameObject Name { get; set; }
+        }
+
+        [DataContract]
+        public class MessageContent
+        {
+            [DataMember(Name = "text")]
+            public string Text { get; set; }
+        }
+    }
+}
